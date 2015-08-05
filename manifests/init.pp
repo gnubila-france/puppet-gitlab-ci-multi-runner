2a6048b00f6158a3f9be293b7605d8e41dd32a4b
Baptiste Grenier <bgrenier@gnubila.fr>
2015-08-05 11:41:56 +0200

    gitlab_ci_multi_runner: just a bit of linting.

--- manifests/init.pp
+++ manifests/init.pp
@@ -31,69 +31,68 @@ class gitlab_ci_multi_runner (
     },
 ) {
     $package_manager = $::osfamily ? {
-        'redhat'  => 'rpm',
-        'debian'  => 'apt',
-        default => 'unknown',
+        'redhat' => 'rpm',
+        'debian' => 'apt',
+        default  => 'unknown',
     }
 
     if $package_manager == 'unknown' {
-        fail("Target Operating system (${operatingsystem}) not supported")
+        fail("Target Operating system (${::operatingsystem}) not supported")
     } elsif $package_manager == 'apt' {
-        warning("${operatingsystem} support is still in Beta - please report any issues to the main repository at https://github.com/frankiethekneeman/puppet-gitlab-ci-multi-runner/issues")
+        warning("${::operatingsystem} support is still in Beta - please report any issues to the main repository at https://github.com/frankiethekneeman/puppet-gitlab-ci-multi-runner/issues")
     }
 
     # Get the file created by the "repo adding" step.
     $repoLocation = $package_manager ? {
-        'rpm'   => "/etc/yum.repos.d/runner_gitlab-ci-multi-runner.repo",
-        'apt'   => "/etc/apt/sources.list.d/runner_gitlab-ci-multi-runner.list",
+        'rpm'   => '/etc/yum.repos.d/runner_gitlab-ci-multi-runner.repo',
+        'apt'   => '/etc/apt/sources.list.d/runner_gitlab-ci-multi-runner.list',
         default => '/var',
-            # Choose a file that will definitely be there so that we don't have to worry about it running in the case
-            # of an unknown package_manager type.
+        # Choose a file that will definitely be there so that we don't
+        # have to worry about it running in the case of an unknown
+        # package_manager type.
     }
-    
-
 
     # Ensure the gitlab_ci_multi_runner user exists.
     # TODO:  Investigate if this is necessary - the install script may handle this.
-    user{ $user:
-        ensure     => "present",
-        managehome => "true",
+    user { $user:
+        ensure     => 'present',
+        managehome => true,
     } ->
     # Add The repository to yum/apt-get
-    exec {"Add Repository":
+    exec { 'Add Repository':
         command  => "curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.${package_manager}.sh | bash",
-        user     => root,
-        provider => shell,
+        user     => 'root',
+        provider => 'shell',
         creates  => $repoLocation,
     } ->
     # Install the package after the repo has been added.
     package { 'gitlab-ci-multi-runner':
         ensure => $version
     } ->
-    exec {"Ensure Service":
-        command  => "gitlab-ci-multi-runner install",
-        user     => root,
-        provider => shell,
-        creates  => "/etc/init.d/gitlab-ci-multi-runner"
+    exec {'Ensure Service':
+        command  => "gitlab-ci-multi-runner install --user $user",
+        user     => 'root',
+        provider => 'shell',
+        creates  => '/etc/init.d/gitlab-ci-multi-runner'
     } ->
     # Ensure that the service is running at all times.
-    service { "gitlab-ci-multi-runner":
-        ensure => "running",
+    service { 'gitlab-ci-multi-runner':
+        ensure => 'running',
     }
 
     if $package_manager == 'rpm' {
-        exec { "Yum Exclude Line":
-            command  => "echo exclude= >> /etc/yum.conf",
+        exec { 'Yum Exclude Line':
+            command  => 'echo exclude= >> /etc/yum.conf',
             onlyif   => "! grep '^exclude=' /etc/yum.conf",
-            user     => root,
-            provider => shell,
-            require  => Exec["Ensure Service"]
-        }->
-        exec { "Yum Exclude gitlab-ci-multi-runner":
+            user     => 'root',
+            provider => 'shell',
+            require  => Exec['Ensure Service']
+        } ->
+        exec { 'Yum Exclude gitlab-ci-multi-runner':
             command  => "sed -i 's/^exclude=.*$/& gitlab-ci-multi-runner/' /etc/yum.conf",
             onlyif   => "! grep '^exclude=.*gitlab-ci-multi-runner' /etc/yum.conf",
-            user     => root,
-            provider => shell,
+            user     => 'root',
+            provider => 'shell',
         }
     }
 }
